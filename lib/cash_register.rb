adcdcddf85c58e5b1c09f3edc1625facce6e004b
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-10 12:39:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+require 'pry'
 
+class CashRegister 
+  attr_accessor :discount, :total
+  
+  def initialize(discount = 0)
+    @total = 0
+    @items = {}
+    @discount = discount 
+  end
+  
+  def add_item(title, price) 
+    @total + price 
+    @items = @items[title => price]
+    binding.pry
+  end 
+  
+end
