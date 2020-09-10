e5ad6a556e4642cf8f74eafacd0ba7cb3c4f098e
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-10 14:04:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -37,7 +37,7 @@ class CashRegister
  
 
  def void_last_transaction
-   self.total = @total - @transactions.pop
+   self.total = @total - @all_transactions.pop
    #last_transaction = @all_transactions.pop
    #@total = @total - last_transaction
   # @total
